ad2f7fcc24a69735cb7c20bfb49659082b1007be
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-03-06 00:02:28 +0600

    ne

--- server2.js
+++ server2.js
@@ -10,35 +10,34 @@ const user =[
 ];
 
 const server = createServer((req, res) => {
+
     if(req.url === '/api/users' && req.method === 'GET'){
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(user));
         res.end();
     }
+
     else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
         const id=req.url.split('/')[5];
         res.setHeader('Content-Type', 'application/json');
         const user = user.find((user) => user.id === parseInt(id));
         if(user){
             res.write(JSON.stringify(user));
-            
-
         }
         else{
             res.statusCode = 404;
             res.write(JSON.stringify({message:"User not found"}));
-
         }
         res.end();
     }
-        
-    
+
     else{
         res.setHeader('Content-Type', 'application/json');
         res.statusCode = 404;
         res.write(JSON.stringify({message:"Route not found"}));
         res.end();
     }
+    
 });
 
 server.listen(PORT , () => {
